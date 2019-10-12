910599249971ac572998636859185843315a7b3c
willmoore288@yahoo.com <willmoore288@yahoo.com>
2019-10-12 17:20:58 -0500

    Add project files.

---
+++ TinkerSwiftApi/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace TinkerSwiftApi.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ TinkerSwiftApi/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace TinkerSwiftApi.Controllers
+{
+    [Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            //I can get userID, username and more with this statement
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            return new string[] { "value1", "value2", userId };
+        }
+
+        // public IEnumerable<string> Get()
+        //public IHttpActionResult Get()
+        //{
+        //    //I can get userID, username and more with this statement
+        //    //string userId = RequestContext.Principal.Identity.GetUserId();
+        //    //return new string[] { "value1", "value2", userId };
+
+        //    string userId = RequestContext.Principal.Identity.GetUserId();
+        //    return Ok(new string[] { "value1", "value2", userId });
+        //}
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
